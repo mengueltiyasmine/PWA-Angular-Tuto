329b2bcaba596c4829b252ff2618a10e32374566
mengueltiyasmine <menguelti.yasmine@gmail.com>
2021-11-01 22:43:22 +0100

    étape 6 et 7

--- tutoriel-angular/src/app/app-routing.module.ts
+++ tutoriel-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {  path: 'hello', component: HelloComponent },
+    {  path: '', component: HelloComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
